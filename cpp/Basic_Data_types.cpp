511c3ff0306d823252a74cc85c1e4f2b11851f27
tigerhooduday <66848419+tigerhooduday@users.noreply.github.com>
2022-02-14 11:48:25 +0530

    Create Basic_Data_types.cpp
    Some C++ data types, their format specifiers, and their most common bit widths are as follows:
    
        Int ("%d"): 32 Bit integer
        Long ("%ld"): 64 bit integer
        Char ("%c"): Character type
        Float ("%f"): 32 bit real value
        Double ("%lf"): 64 bit real value
    --------------------------------------------------
    
    Sample Input
    
    3 12345678912345 a 334.23 14049.30493
    -------------------------------
    Sample Output
    
    3
    12345678912345
    a
    334.230
    14049.304930000

---
+++ cpp/Basic_Data_types.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include<stdio.h>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    long a;
+    long long int b;
+    char c;
+    float d;
+    double e;
+
+    cin>>a>>b>>c>>d>>e;
+    printf("%d\n%lld\n%c\n%f\n%lf",a,b,c,d,e);
+
+    return 0;
+}
